025e0bcc4d7b13befb47b891bc43908843312ce2
your_username <dontlookbackinanger@yandex.ru>
2022-12-23 00:10:02 +0300

    fix table

--- src/store/index.js
+++ src/store/index.js
@@ -24,7 +24,7 @@ const rootReducer = combineReducers(
 const middleware = [thunk];
 const store = createStore(rootReducer,{},compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     
     ));
 
